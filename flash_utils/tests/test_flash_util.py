df182c7bf722dd88ec482ef5b486838dc7076e1d
Lucas Keller <kellerjlucas@gmail.com>
2023-10-03 14:36:46 -0500

    Mock Serial Port
    Mock serial port for github actions (and future serial port tests).

--- flash_utils/tests/test_flash_util.py
+++ flash_utils/tests/test_flash_util.py
@@ -3,10 +3,20 @@ This module contains unit tests for the FlashUtil class in the flash_utils.flash
 """
 
 import sys
+from unittest.mock import Mock
 
 import pytest
 
 from flash_utils.flash import FlashUtil
+
+
+@pytest.fixture(name="mock_serial_port")
+def fixture_serial_port(monkeypatch):
+    """Mock the serial port for testing. Useful for testing on github actions."""
+
+    serial_mock = Mock()
+    monkeypatch.setattr("flash_utils.flash.serial.Serial", serial_mock)
+    return serial_mock
 
 
 @pytest.mark.parametrize("option", ("-h", "--help"))
@@ -44,7 +54,9 @@ def test_bad_bootloader_param(capsys: pytest.CaptureFixture[str], flash_options)
 
 
 @pytest.mark.parametrize("flash_option", ("--bootloader", "--rootfs", "--full"))
-def test_good_params_missing_images(capsys: pytest.CaptureFixture[str], flash_option):
+def test_good_params_missing_images(
+    capsys: pytest.CaptureFixture[str], flash_option, mock_serial_port
+):
     """Test FlashUtil with good parameters, but missing flash files"""
 
     with pytest.raises(SystemExit):
@@ -52,6 +64,7 @@ def test_good_params_missing_images(capsys: pytest.CaptureFixture[str], flash_op
         sys.argv = ["flash_util.py", flash_option]
         FlashUtil()
     output = capsys.readouterr()
+    mock_serial_port.assert_called_once_with(port="/dev/ttyUSB0", baudrate=115200)
 
     assert output is not None
     assert "missing" in output.err.lower() and "image" in output.err.lower()
